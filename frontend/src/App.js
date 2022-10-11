13bd6e187a773305e31ff7f61c047df8e8dd4c02
Cletus Ogah <ogahcletus@gmail.com>
2022-10-11 14:56:05 +0100

    implemented codes for bootstrap react routrss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,39 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import Productpage from './Pages/Productpage';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap'
+
 
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-       <Link to={"/"}>kleitos</Link>
+    <div className='site-container d-flex flex-column'>
+      <header>
+      <Navbar bg='dark' variant='dark'>
+      <Container>
+      <LinkContainer to='/'>
+      <Navbar.Brand>kleitos</Navbar.Brand>
+      </LinkContainer>
+        
+      </Container>
+
+      </Navbar>
+       
       </header>
       <main>
+      <Container>
       <Routes>
         <Route path="/" element={< Homepage />} />
         <Route path="/product/:slug" element={<Productpage/>} />
       </Routes>
-        
+      </Container>  
       </main>
+      <footer>
+        <div className='text-center'>All rights reserved</div>
+      </footer>
     </div>
     </BrowserRouter>
   );
